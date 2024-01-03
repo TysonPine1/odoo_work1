e45753629408469089093b122d3295b384b35827
unknown <kaungthawmaung91@gmail.com>
2024-01-03 13:11:04 +0630

    WR-2s changes

--- open_academy/wizard/wizard.py
+++ open_academy/wizard/wizard.py
@@ -1,4 +1,4 @@
-from odoo import api, fields, models
+from odoo import fields, models, api
 
 class CreateAttendee(models.TransientModel):
     _name = "create.wizard"
@@ -9,5 +9,38 @@ class CreateAttendee(models.TransientModel):
     course_id = fields.Many2one("openacademy.course",'Course')
     date = fields.Date(string='Date')
     
-   
-            
+    def action_wizport(self):
+    
+        data = {
+            'model': 'create.wizard',
+            'session_id': self.session_id.id,
+            'partner_id': self.partner_id.ids,
+            'date': self.date,
+            'form': self.read()[0]
+        }
+
+        selected_session_id = data['form']['session_id'][0]
+        selected_partner_ids = data['form']['partner_id']
+        print("SELECTED____>", selected_partner_ids)
+        sessions = self.env['openacademy.session'].search([('id', '=' , selected_session_id)]).filtered(lambda o: o.attendees)
+        
+
+        session_list = []
+        for s in sessions:
+            attendees_list = []
+            for p in s.attendees:
+                # vals = {
+                #     'attendees':p.name
+                # }
+                attendees_list.append(p.name)
+            vals = {
+                'session_name':s.session_name,
+                'attendees':attendees_list,
+                'start_date':s.start_date,
+            }
+        
+            session_list.append(vals)
+        data['sessions']=session_list
+                
+        return self.env.ref('open_academy.action_report_wiz').with_context(landscape=True).report_action(self, data=data)
+    

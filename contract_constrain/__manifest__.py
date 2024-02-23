718e842e7145cf3faf2f76a023bb1871816e38a8
Kaung Thaw Maung <kaung.thawmaung@digipowermm.com>
2024-02-23 09:35:56 +0630

    contract initial commit

---
+++ contract_constrain/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Inherit WorkPlace 2',
+    'version': '13.0',
+    'summary': 'Contact Work Inherit',
+    'sequence': 3,
+    'depends': ['mail', 'base', 'board', 'web', 'contacts'],
+    'category': 'Work Related',
+    'license': 'AGPL-3',
+    'author': 'Heinrich von Helmholtz',
+    'data': [
+        
+    ],
+    'demo': [],
+    'application': True,
+    'installable': True,
+    'auto_install': False
+}
